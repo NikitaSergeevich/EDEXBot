95d9629adeb32394246c2e9446e71de31a0c3a0d
NikitaSergeevich <dercoebo@mail.ru>
2017-07-02 08:59:59 +0300

    morning commit

--- BlockchainHack/Program.cs
+++ BlockchainHack/Program.cs
@@ -6,8 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-
-using Telegram.Bot;
+ using Microsoft.CodeAnalysis.CSharp.Syntax;
+ using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -20,11 +20,15 @@ namespace BlockchainHack
 {
     public class Program
     {
+        
         private static readonly TelegramBotClient Bot = new TelegramBotClient("408098530:AAGKKN-C7R2wbLcIqE0T17v7RZeNO25I8fQ");
+
+        static Dictionary<long, Dictionary<String, List<Telegram.Bot.Types.Contact>>> contactsdict = new Dictionary<long, Dictionary<String, List<Telegram.Bot.Types.Contact>>>();
+        static Dictionary<long, string> statedict = new Dictionary<long, string>();
+        static Dictionary<long, string> docnamedict = new Dictionary<long, string>();
         
         public static void Main(string[] args)
         {
-          
             /*var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -64,36 +68,6 @@ namespace BlockchainHack
         private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
         {
             Console.WriteLine("inlinequery");
-            InlineQueryResult[] results = {
-                new InlineQueryResultLocation
-                {
-                    Id = "1",
-                    Latitude = 40.7058316f, // displayed result
-                    Longitude = -74.2581888f,
-                    Title = "New York",
-                    InputMessageContent = new InputLocationMessageContent // message if result is selected
-                    {
-                        Latitude = 40.7058316f,
-                        Longitude = -74.2581888f,
-                    }
-                },
-
-                new InlineQueryResultLocation
-                {
-                    Id = "2",
-                    Longitude = 52.507629f, // displayed result
-                    Latitude = 13.1449577f,
-                    Title = "Berlin",
-                    InputMessageContent = new InputLocationMessageContent // message if result is selected
-                    {
-                        Longitude = 52.507629f,
-                        Latitude = 13.1449577f
-                    }
-                }
-            };
-            
-
-            await Bot.AnswerInlineQueryAsync(inlineQueryEventArgs.InlineQuery.Id, results, isPersonal: true, cacheTime: 0);
         }
 
         private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
@@ -118,20 +92,50 @@ namespace BlockchainHack
                     }
                 };
             rkm.OneTimeKeyboard = true;
+
+            long tgid = messageEventArgs.Message.Chat.Id;
+            string docname = null;
+            Contact contact = null;
+            if (messageEventArgs.Message.Type.Equals(MessageType.DocumentMessage)){
+                docname = messageEventArgs.Message.Document.FileName;
+            } else if (messageEventArgs.Message.Type.Equals(MessageType.ContactMessage))
+            {
+                contact = messageEventArgs.Message.Contact;
+            }
             
             if (messageEventArgs.Message.Type.Equals(MessageType.TextMessage) && 
                 messageEventArgs.Message.Text.Equals("/start")){
+                Console.WriteLine("Init");
+                statedict[tgid] = "init";
+                docnamedict = new Dictionary<long, string>();
+                contactsdict = new Dictionary<long, Dictionary<string, List<Contact>>>();
                 await Bot.SendTextMessageAsync(messageEventArgs.Message.Chat.Id, "Please Select An Option", false, false, 0, rkm );
             } else if (messageEventArgs.Message.Type.Equals(MessageType.TextMessage) && 
                        messageEventArgs.Message.Text.Equals("Upload")){
-                await Bot.SendTextMessageAsync(messageEventArgs.Message.Chat.Id, "Now upload file you want to sign", false, false, 0, null);
-            } else if (messageEventArgs.Message.Type.Equals(MessageType.DocumentMessage)){
+                Console.WriteLine("Want to upload");
+                await Bot.SendTextMessageAsync(messageEventArgs.Message.Chat.Id, "Now upload file you want to sign", false, false, 0, new ReplyKeyboardMarkup());
+            } else if (messageEventArgs.Message.Type.Equals(MessageType.DocumentMessage) && 
+                       statedict[tgid] == "init"){
+                Console.WriteLine("Document received");
+                
+                //Update Data
+                statedict[tgid] = "uploaded";
+                docnamedict[tgid] = docname;
+                
+                //Send Reply
                 await Bot.SendTextMessageAsync(messageEventArgs.Message.Chat.Id, "Please select document signers by phone number from your contact list or by alias", false, false, 0, null );
+                
+            } else if (messageEventArgs.Message.Type.Equals(MessageType.ContactMessage) && 
+                       statedict[tgid] == "uploaded")
+            {
+                statedict[messageEventArgs.Message.Chat.Id] = "set";
+                string s = docnamedict[tgid];
+                List<Contact> t = contactsdict[messageEventArgs.Message.Chat.Id][s];
+                t.Add(contact);
             }
         }
 
-        private static async void BotOnCallbackQueryReceived(object sender,
-            CallbackQueryEventArgs callbackQueryEventArgs)
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
         {
             await Bot.AnswerCallbackQueryAsync(callbackQueryEventArgs.CallbackQuery.Id,
                 $"Received {callbackQueryEventArgs.CallbackQuery.Data}");
